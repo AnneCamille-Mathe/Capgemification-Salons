aaf297988da2bd6e083174ec94ec2b8f7b874c4e
Annek <anne.camile.mathe@gmail.com>
2019-09-10 12:29:51 +0200

    CanvasSynopsisOkToDoScore&Timer&Minijeu

--- Assets/Script/Boutons/ButtonDebutScript.cs
+++ Assets/Script/Boutons/ButtonDebutScript.cs
@@ -8,17 +8,20 @@ public class ButtonDebutScript : MonoBehaviour
     //Variables
     public GameObject CanevasScore;
     public GameObject CanevasTimer;
-    public GameObject CanevasDebut;
+    public GameObject CanvasJoystick;
+    public GameObject CanvasSynospis;
 
     //On regarde combien de fois l'utilisateur a cliqué
     public int count;
-    //TODO - Message du début
     
     // Start is called before the first frame update
     void Start()
     {
         this.CanevasTimer.SetActive(false);
         this.CanevasScore.SetActive(false);
+        this.CanvasJoystick.SetActive(false);
+        this.CanvasSynospis.SetActive(true);
+        //L'utilisateur clique la première fois
         this.count = 1;
     }
 
@@ -30,11 +33,13 @@ public class ButtonDebutScript : MonoBehaviour
 
     public void onClic()
     {
+        //L'utilisateur a cliqué une fois --> la suite du message s'affiche
         if (this.count == 1)
         {
             StartCoroutine(this.change());
             this.count = 2;
         }
+        //L'utilisateur a cliqué une deuxième fois --> le canvas disparait
         else
         {
             StartCoroutine(this.desepear());
@@ -43,16 +48,17 @@ public class ButtonDebutScript : MonoBehaviour
 
     IEnumerator change()
     {
-        GameObject.Find("Canvas_debut/Panel_debut/Text_debut").GetComponent<Text>().text = "Les codeurs vous ont parfois indique le chemin a suivre, regardez bien le sol … Votre mission est la suivante : penetrez les lieux, explorez, reparez les projets… Mais attention ! L’IA a verrouille les portes et bloque certaines issues, vous allez devoir dejouer ses plans pour reussir votre mission !";
+        GameObject.Find("CanvasSynopsis/Panel/Text").GetComponent<Text>().text = "Les codeurs vous ont parfois indique le chemin a suivre, regardez bien le sol … Votre mission est la suivante : penetrez les lieux, explorez, reparez les projets… Mais attention ! L’IA a verrouille les portes et bloque certaines issues, vous allez devoir dejouer ses plans pour reussir votre mission !";
         yield return new WaitForSeconds(1);
     }
 
     IEnumerator desepear()
     {
         yield return new WaitForSeconds(1);
-        this.CanevasDebut.SetActive(false);
         this.CanevasTimer.SetActive(true);
         this.CanevasScore.SetActive(true);
+        this.CanvasJoystick.SetActive(true);
+        this.CanvasSynospis.SetActive(false);
     }
 
 }
